74b61b3c487f767ea7ce45debd53b0b3ed62513a
Avi Gawande <115152362+AviGawande@users.noreply.github.com>
2023-04-24 21:45:43 +0530

    commit

---
+++ App/Quiz_Game.py
@@ -0,0 +1,121 @@
+questions = [{
+
+	'question' : ' Which law of newton states about inertia and state of condition in physics? ',
+	'options'  : [1,2,3,4],
+	'correct_ans' : 2
+},
+{
+
+	'question' : 'Which city in india is know as pink city?',
+	'options'  : ['dehradun','jaipur','raipur','mohali'],
+	'correct_ans' : 'jaipur'
+},
+{
+
+	'question' : 'The Kishkindha is located in which state of india?',
+	'options'  : ['tamil nadu', 'odisha', 'Karnataka', 'kerala'],
+	'correct_ans' : 'Karnataka'
+},
+{
+
+	'question' : 'What is the national flower of India?',
+	'options'  : ['Lotus','Rhododendron','Golden Shower','Not declared'],
+	'correct_ans' : 'Lotus'
+},
+{
+
+	'question' : 'Which of the following states is not located in the North?',
+	'options'  : ['Jharkhand', 'Jammu and Kashmir', 'Himachal Pradesh', 'Haryana'],
+	'correct_ans' : 'Jharkhand'
+},
+{
+
+	'question' : 'In which of the following state, the main language is Khasi?',
+	'options'  : ['Mizoram', 'Nagaland', 'Meghalaya', 'Tripura'],
+	'correct_ans' : 'Meghalaya'
+},
+{
+
+	'question' : 'Which is the largest gold reserve of india?',
+	'options'  : ['Kerala', 'hampi', 'kolar', 'Arunachal Pradesh'],
+	'correct_ans' : 'kolar'
+},
+{
+
+	'question' : ' Which state has the largest area?',
+	'options'  : ['Maharashtra', 'Madhya Pradesh', 'Uttar Pradesh', 'Rajasthan'],
+	'correct_ans' : 'Rajasthan'
+},
+{
+
+	'question' : 'Which state has the largest population?',
+	'options'  : ['Uttar Pradesh', 'Maharastra', 'Bihar', 'Andra Pradesh'],
+	'correct_ans' : 'Uttar Pradesh'
+},
+{
+
+	'question' : ' In what state is Elephant Falls located?',
+	'options'  : ['Mizoram', 'Orissa', 'Manipur', 'Meghalaya'],
+	'correct_ans' : 'Meghalaya'
+},
+{
+
+	'question' : ' Which of the following device converts chemical energy in to electrical energy?',
+	'options'  : ['Battery', 'Loud Speaker', 'Solar Cell', 'Electric Motor'],
+	'correct_ans' : 'Battery'
+},
+{
+
+	'question' : 'The energy possessed by a body due to its position is called:',
+	'options'  : ['Kinetic Energy', 'Potential Energy', 'Mechanical Energy', 'Electrical Energy'],
+	'correct_ans' : 'Potential Energy'
+},
+{
+
+	'question' : 'Joule is a unit of:',
+	'options'  : ['Work', 'Power', 'Momentum', 'None of the above'],
+	'correct_ans' : 'Work'
+},
+{
+
+	'question' : ' The commercial unit of Energy is:',
+	'options'  : ['Watt', 'Watt-hour', 'Kilowatt-hour', 'Kilowatt'],
+	'correct_ans' : 'Kilowatt-hour'
+},
+{
+
+	'question' : 'When an object falls freely towards the ground, then its total energy:',
+	'options'  : ['Increases', 'Decreases', 'Remains constant', 'First increases then decreases'],
+	'correct_ans' : 'Remains constant'
+},
+{
+
+	'question' : 'In 1 minute how much energy does a 100 W electric bulb transfers?',
+	'options'  : ['100 J', '600 J', '3600 J', '6000 J'],
+	'correct_ans' : '6000 J'
+},
+{
+
+	'question' : 'What happens to the body on which work is done:',
+	'options'  : ['It loses energy', 'It gains energy', 'No change in the energy', 'First it loses then it gain'],
+	'correct_ans' : 'It gains energy'
+},
+{
+
+	'question' : 'A radio set of 60 watts runs for 50 hours. How many units of electrical energy are consumed in kWh?',
+	'options'  : ['2 kWh', '3 kWh', '4 kWh', '2.5 kWh'],
+	'correct_ans' : '3 kWh'
+},
+{
+
+	'question' : 'What is the smallest unit of power?',
+	'options'  : ['Watt', 'Kilowatt', 'Horse power', 'Milliwatt'],
+	'correct_ans' : 'Watt'
+},
+{
+
+	'question' : 'On an object the work done does not depend upon:',
+	'options'  : ['Displacement', 'Angle between force and displacement', 'Force applied', 'Initial velocity of an object'],
+	'correct_ans' : 'Initial velocity of an object'
+}
+]
